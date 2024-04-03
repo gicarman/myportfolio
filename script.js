755c85aa35415548dcd86738bc2289ffd1df65d6
gicarman <123538772+gicarman@users.noreply.github.com>
2024-04-03 20:16:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ document.getElementById('hamburger').addEventListener('click', function() {
     var navList = document.getElementById('nav-list');
     navList.classList.toggle('active');
 });
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+});
+
